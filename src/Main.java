c13ae9d7c18bc43100c6d008aa641f3cbcd2290b
유지민 <103996321+wlals1126@users.noreply.github.com>
2022-10-14 11:11:17 +0900

    Baekjoon 1427

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int cnt = n;
+        char[] arr = sc.nextLine().toCharArray();
+        Arrays.sort(arr); // 리스트 정렬
 
-        for(int i = 0; i < n; i++) {
-            String word = sc.next();
-            boolean a[] = new boolean[26];
-
-            for(int j = 0; j < word.length()-1; j++) {
-                if(word.charAt(j) != word.charAt(j+1)) {
-                    if(a[word.charAt(j+1)-97] == true) {
-                        cnt--;
-                        break;
-                    }
-                }
-                a[word.charAt(j)-97] = true;
-            }
+        for(int i = arr.length-1; i >= 0; i--) {
+            System.out.print(arr[i]);
         }
-        System.out.println(cnt);
     }
 }
