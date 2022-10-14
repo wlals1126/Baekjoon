f24ebf30d04bd4912ce3cfa8b3ecdea62867c354
유지민 <103996321+wlals1126@users.noreply.github.com>
2022-10-14 10:51:01 +0900

    Baekjoon 1316

---
+++ src/Baekjoon1316/Main.java
@@ -0,0 +1,30 @@
+package Baekjoon1316;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = n;
+
+        for(int i = 0; i < n; i++) {
+            String word = sc.next();
+            boolean a[] = new boolean[26];
+
+            for(int j = 0; j < word.length()-1; j++) {
+                if(word.charAt(j) != word.charAt(j+1)) {
+                    if(a[word.charAt(j+1)-97] == true) {
+                        cnt--;
+                        break;
+                    }
+                }
+                a[word.charAt(j)-97] = true;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
