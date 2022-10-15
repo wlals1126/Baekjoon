5327e3fc3aa216655bb5e74a4f1aa08076d00958
유지민 <103996321+wlals1126@users.noreply.github.com>
2022-10-15 09:43:53 +0900

    Baekjoon 1978

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.StreamTokenizer;
-import java.nio.Buffer;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
-//https://velog.io/@suw0n/11723JAVA
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n, m = Integer.parseInt(br.readLine());
-        boolean[] arr = new boolean[20];
-        String s;
-        StringBuilder sb = new StringBuilder();
-        while (m > 0) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            s = st.nextToken();
-            if(s.equals("all") || s.equals("empty")) {
-                if (s.contains("a"))
-                    for (int i = 0; i < 20; i++)
-                        arr[i] = true;
-                else
-                    for (int i = 0; i < 20; i++)
-                        arr[i] = false;
-            }
-            else {
-                n = Integer.parseInt(st.nextToken());
-                if(s.contains("a"))
-                    arr[n-1] = true;
-                else if(s.contains("r"))
-                    arr[n-1] = false;
-                else if(s.contains("c"))
-                    sb.append((arr[n-1] == true ? 1 : 0) + "\n");
-                else arr[n-1] = !arr[n-1];
-            }
-            m--;
+    public static boolean Prime (int n) {
+        if(n < 2) return false;
+        for(int i = 2; i*i <= n; i++) {
+            if(n%i==0) return false;
         }
-        System.out.println(sb);
+        return true;
+    }
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int res = 0;
+        for(int i = 0; i < m; i++) {
+            int num = sc.nextInt();
+            if(Prime(num)) res++;
+        }
+        System.out.println(res);
     }
 }
