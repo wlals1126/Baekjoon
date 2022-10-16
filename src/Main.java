fd49f71f22f549feebfbb1ccde748cf501736e2f
유지민 <103996321+wlals1126@users.noreply.github.com>
2022-10-16 21:40:02 +0900

    Baekjoon 1769

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,39 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.Buffer;
 import java.util.Scanner;
 
+// 옆집삼식이
 public class Main {
-    public static boolean Prime (int n) {
-        if(n < 2) return false;
-        for(int i = 2; i*i <= n; i++) {
-            if(n%i==0) return false;
-        }
-        return true;
-    }
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        int m = sc.nextInt();
+        String str = sc.next();
         int res = 0;
-        for(int i = 0; i < m; i++) {
-            int num = sc.nextInt();
-            if(Prime(num)) res++;
+        int cnt = 0;
+        int llen = str.length();
+        if(llen != 1) {
+            while(true) {
+                int len = str.length();
+                if(len == 1)
+                    break;
+                res = 0;
+                for(int i = 0; i < len; i++) {
+                    char ch = str.charAt(i);
+                    res += Integer.parseInt(String.valueOf(ch));
+                }
+                str = String.valueOf(res);
+                cnt++;
+            }
         }
-        System.out.println(res);
+        else {
+            res = Integer.parseInt(str);
+        }
+        System.out.println(cnt);
+        if(res % 3 == 0){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
     }
 }
