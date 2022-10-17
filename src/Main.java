29acd2973b83e88f3f5863f665d62f981bfdcb15
유지민 <103996321+wlals1126@users.noreply.github.com>
2022-10-17 22:04:24 +0900

    Baekjoon 10950

--- src/Main.java
+++ src/Main.java
@@ -8,32 +8,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        int res = 0;
-        int cnt = 0;
-        int llen = str.length();
-        if(llen != 1) {
-            while(true) {
-                int len = str.length();
-                if(len == 1)
-                    break;
-                res = 0;
-                for(int i = 0; i < len; i++) {
-                    char ch = str.charAt(i);
-                    res += Integer.parseInt(String.valueOf(ch));
-                }
-                str = String.valueOf(res);
-                cnt++;
-            }
-        }
-        else {
-            res = Integer.parseInt(str);
-        }
-        System.out.println(cnt);
-        if(res % 3 == 0){
-            System.out.println("YES");
-        }else{
-            System.out.println("NO");
+        int n = sc.nextInt();
+        int a, b;
+        for(int i = 0; i < n; i++) {
+            a = sc.nextInt();
+            b = sc.nextInt();
+            System.out.printf("%d\n", a+b);
         }
     }
 }
