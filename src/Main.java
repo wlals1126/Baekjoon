c08c062017a7e82716f98c726c2e29048d096363
유지민 <103996321+wlals1126@users.noreply.github.com>
2022-10-18 20:46:26 +0900

    Baekjoon 2750

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.Buffer;
+import java.util.Arrays;
 import java.util.Scanner;
 
-// 옆집삼식이
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int a, b;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
         for(int i = 0; i < n; i++) {
-            a = sc.nextInt();
-            b = sc.nextInt();
-            System.out.printf("%d\n", a+b);
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        for(int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
         }
     }
 }
