f2e84d4060f430c32aff6534bfbb8ec15ee36a23
유지민 <103996321+wlals1126@users.noreply.github.com>
2022-10-20 10:12:42 +0900

    Baekjoon 2751

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,24 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.Buffer;
+import java.io.*;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-        for(int i = 0; i < n; i++) {
+        Integer[] arr = new Integer[n];
+        for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
         Arrays.sort(arr);
-        for(int i = 0; i < n; i++) {
-            System.out.println(arr[i]);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(arr[i] + "\n");
         }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
