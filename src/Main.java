f1205960fa23dd04e15a32dfe8741844203264bd
유지민 <103996321+wlals1126@users.noreply.github.com>
2022-11-24 21:05:55 +0900

    Baekjoon 1158 (11.24)

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,20 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
         Queue<Integer> q = new LinkedList<>();
-        for(int i = 1; i <=n; i++) {
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        for(int i = 1; i <= n; i++) {
             q.add(i);
         }
+        System.out.print("<");
         while(q.size() > 1) {
-            q.remove();
-            q.add(q.poll());
+            for(int i = 1; i < k; i++) {
+                q.add(q.poll());
+            }
+            System.out.print(q.poll() + ", ");
         }
-        System.out.print(q.poll());
+        System.out.println(q.poll() + ">");
     }
 }
