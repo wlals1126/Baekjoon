9e10b60c73965c399c852a4a20690921111b2ebc
유지민 <103996321+wlals1126@users.noreply.github.com>
2022-12-14 21:54:02 +0900

    Baekjoon 7568

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,24 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        Queue<Integer> q = new LinkedList<>();
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        for(int i = 1; i <= n; i++) {
-            q.add(i);
+        int n = Integer.parseInt(br.readLine());
+        int temp = 1;
+        int[][] arr = new int[50][200];
+        String[] st;
+        for(int i = 0; i < n; i++) {
+            st = br.readLine().split(" ");
+            arr[i][0] = Integer.parseInt(st[0]);
+            arr[i][1] = Integer.parseInt(st[1]);
         }
-        System.out.print("<");
-        while(q.size() > 1) {
-            for(int i = 1; i < k; i++) {
-                q.add(q.poll());
+        for(int i = 0; i < n; i++) {
+            temp = 1;
+            for(int j = 0; j < n; j++) {
+                if(i == j) continue;
+                if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
+                    temp++;
+                }
             }
-            System.out.print(q.poll() + ", ");
+            System.out.print(temp + " ");
         }
-        System.out.println(q.poll() + ">");
     }
 }
